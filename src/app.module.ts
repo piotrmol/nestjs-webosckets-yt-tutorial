d4a86741ffd298cd1b783ab50a6456734bb0d0bd
Piotr Mol <piotr94mol@gmail.com>
2022-12-11 12:50:36 +0100

    feat: setup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { WebsocketsModule } from './websockets/websockets.module';
 
 @Module({
   imports: [
-    WebsocketsModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'static'),
     }),
